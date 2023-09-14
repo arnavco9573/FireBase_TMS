35009aec8b0492131c888177d56f2444e95d01a9
arnavco9573 <arnavsharm589@gmail.com>
2023-09-15 04:34:33 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -320,7 +320,7 @@ const Home = () => {
 
         <button
           type="button"
-          className=" mt-3  w-32 bg-gradient-to-r from-cyan-500 to-blue-500 text-white py-2 rounded-lg"
+          className=" mt-3 w-32 bg-gradient-to-r from-cyan-500 to-blue-500 text-white py-2 rounded-lg"
           onClick={logout}
         >
           LogOut
